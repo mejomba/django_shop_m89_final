803ad53257300092d775fc070ca72e1ccab13504
mejomba <moojaminzadeh@gmail.com>
2023-05-18 21:55:39 +0330

    add discount and address test

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,13 +1,24 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
+from django.utils import timezone
+
+from core.models import Address, Discount
 
 
 def test_user(email='test@mail.com', password='Test1234', extra={}):
     return get_user_model().objects.create_user(email, password, **extra)
 
 
-class ModelTest(TestCase):
+def test_address(user, extra={}):
+    return Address.objects.create(user=user, **extra)
+
+
+def test_discount(extra):
+    return Discount.objects.create(**extra)
+
+
+class UserModelTest(TestCase):
     
     def test_create_user_with_complet_data(self):
         
@@ -77,4 +88,55 @@ class ModelTest(TestCase):
         
         with self.assertRaises(IntegrityError):
             new_user = get_user_model().objects.create_user('new@mail.com', 'Test1234', phone=user.phone)
+    
+    def test_user_str(self):
+        user = test_user()
+        self.assertEqual(user.email, 'test@mail.com')
         
+        
+class AddressModelTest(TestCase):
+    
+    def test_address_str(self):
+        extra = {'country':'ایران',
+                'province': 'تهرا',
+                'city': 'شهر',
+                'street': 'خیابان',
+                'zip_code': '1234567890',
+                'pelak': '1234',
+                'full_address': 'آدرس کامل شامل یک متن',}
+        
+        address = test_address(user=test_user(), extra=extra)
+        self.assertEqual(str(address), address.user.full_name)
+        
+
+class DiscountModelTest(TestCase):
+    
+    def setUp(self) -> None:
+        self.extra = {'percent': '10', 
+                    'mablagh': '1000', 
+                    'start_date': timezone.now(), 
+                    'end_date': timezone.now()+timezone.timedelta(1)}
+        
+    def test_discount_str(self):
+        
+        discount = Discount.objects.create(name='test', code='1234', **self.extra)
+        self.assertEqual(str(discount), discount.code)
+        
+    def test_discount_unique_name(self):
+        
+        discount = Discount.objects.create(name='test', code='1234', **self.extra)
+        with self.assertRaises(IntegrityError):
+            Discount.objects.create(name='test', code='4321', **self.extra)
+            
+    def test_discount_unique_code(self):
+        
+        discount = Discount.objects.create(name='test1', code='1234', **self.extra)
+        with self.assertRaises(IntegrityError):
+            Discount.objects.create(name='test2', code='1234', **self.extra)
+            
+    def test_discount_percent_mablagh_pair(self):
+        
+        self.extra.pop('percent')
+        self.extra.pop('mablagh')
+        with self.assertRaises(IntegrityError):
+            discount = Discount.objects.create(name='test1', code='1234', **self.extra)
