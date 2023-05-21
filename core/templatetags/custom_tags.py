51f4304e17120b06586025454a7e9c25e7358fe0
mejomba <moojaminzadeh@gmail.com>
2023-05-22 00:55:22 +0330

    add show_product_basic_info, show_product_action, show_product_full_info, show_product_comments

--- core/templatetags/custom_tags.py
+++ core/templatetags/custom_tags.py
@@ -10,5 +10,27 @@ def show_single_product(single_product_object):
     
 
 @register.inclusion_tag('shop/partial/post_image_gallery.html')
-def show_image_gallery(images):
-    return {'images': images}
+def show_image_gallery(thumbnail, images):
+    img = [i.image for i in images]
+    img.insert(0, thumbnail)
+    return {'images': img}
+
+
+@register.inclusion_tag('shop/partial/product_basic_info.html')
+def show_product_basic_info(product):
+    return {'product': product}
+
+
+@register.inclusion_tag('shop/partial/product_action.html')
+def show_product_action(product):
+    return {'product': product}
+
+
+@register.inclusion_tag('shop/partial/product_full_info.html')
+def show_product_full_info(product):
+    return {'product': product}
+
+
+@register.inclusion_tag('shop/partial/product_comments.html')
+def show_product_comments(comments):
+    return {'comments': comments}
