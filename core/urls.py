abd053209facc15eaa782fc11b0f69f437230f65
mejomba <moojaminzadeh@gmail.com>
2023-05-25 23:51:41 +0330

    add new url for api and template views

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    # path('profile/<int:pk>/', views.Profile.as_view(), name='profile'),
+    path('profile/', views.Profile.as_view(), name='profile')
+]
