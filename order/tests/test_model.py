fffb1d2670cb8a2325118c389293dd22d27f27f5
mejomba <moojaminzadeh@gmail.com>
2023-05-25 23:36:56 +0330

    hotfix for model test

--- order/tests/test_model.py
+++ order/tests/test_model.py
@@ -4,95 +4,88 @@ from django.db.utils import IntegrityError
 from django.utils import timezone
 import datetime
 
+from core.models import Address
 from order.models import Cart, CartItem, Order, OrderItem, Transaction
 from shop.models import Product
-from core.tests.test_models import test_address
-
-
-
-def test_user(email='test@mail.com', password='Test1234', extra={}):
-    return get_user_model().objects.create_user(email, password, **extra)
-
-
-def test_cart():
-    return Cart.objects.create(user=test_user())
-
-
-def test_product():
-    return Product.objects.create(name='test', price=1000, quantity=1)
-
-
-def test_order():
-    return Order.objects.create(address=test_address(test_user()), user=test_user(email='a@m.com'))
-
-
-def test_order_item():
-    return OrderItem(count=1, product=test_product(), order=test_order())
-    
-
-def test_transaction():
-    return Transaction.objects.create(user=test_user('b@c.com'), order=test_order(), transaction_code='1234', total_price=1)
 
 
 class CartModelTest(TestCase):
+    def setUp(self) -> None:
+        self.test_user = get_user_model().objects.create_user('test@mail.com', 'Test1234')
+        self.test_cart = Cart.objects.create(user=self.test_user)
     
     def test_cart_str(self):
-        cart = test_cart()
+        cart = self.test_cart
         self.assertEqual(str(cart), cart.user.email)
         
         
 class CartItemModelTest(TestCase):
-    
-    def test_cart_item_str(self):    
-        cart_item = CartItem.objects.create(count=1, 
-                                            cart=test_cart(),
-                                            product=test_product()
+    def setUp(self) -> None:
+        self.test_user = get_user_model().objects.create_user('test@mail.com', 'Test1234')
+        self.test_cart = Cart.objects.create(user=self.test_user)
+        self.test_product = Product.objects.create(name='test', price=1000, quantity=1)
+
+    def test_cart_item_str(self):
+        cart_item = CartItem.objects.create(count=1,
+                                            cart=self.test_cart,
+                                            product=self.test_product
                                             )
         self.assertEqual(str(cart_item), f'{cart_item.cart} : {cart_item.product}')
-        
+
     def test_cart_item_count(self):
         with self.assertRaises(IntegrityError):
-            cart_item = CartItem.objects.create(count=-1, 
-                                                cart=test_cart(),
-                                                product=test_product()
-                                                )
+            CartItem.objects.create(count=-1,
+                                    cart=self.test_cart,
+                                    product=self.test_product
+                                    )
             
             
 class OrderModelTest(TestCase):
+    def setUp(self) -> None:
+        self.test_user = get_user_model().objects.create_user('test@mail.com', 'Test1234')
+        self.test_address = Address.objects.create(user=self.test_user)
+        self.test_order = Order.objects.create(address=self.test_address, user=self.test_user)
     
-    
-    def test_order_time_for_pay(self):        
-        order = test_order()        
+    def test_order_time_for_pay(self):
+        order = self.test_order
         self.assertEqual(order.time_for_pay.date(), (timezone.now() + datetime.timedelta(minutes=30)).date())
         
     def test_order_str(self):        
-        order = test_order()        
+        order = self.test_order
         self.assertEqual(str(order), f'{order.user} : {order.status}')
         
     def test_order_status(self):        
-        order = test_order()        
+        order = self.test_order
         self.assertEqual(order.status, '1')
         
 
 class OrderItemModelTest(TestCase):
-    
-        
-    def test_order_item_str(self):        
-        order_item = test_order_item()        
+    def setUp(self) -> None:
+        self.test_user = get_user_model().objects.create_user('test@mail.com', 'Test1234')
+        self.test_product = Product.objects.create(name='test', price=1000, quantity=1)
+        self.test_address = Address.objects.create(user=self.test_user)
+        self.test_order = Order.objects.create(address=self.test_address, user=self.test_user)
+        self.test_order_item = OrderItem(count=1, product=self.test_product, order=self.test_order)
+
+    def test_order_item_str(self):
+        order_item = self.test_order_item
         self.assertEqual(str(order_item), f'{order_item.order} : {order_item.product}')
-        
+
     def test_order_item_count(self):
         with self.assertRaises(IntegrityError):
-            order_item = OrderItem.objects.create(count=-1, 
-                                                order=test_order(),
-                                                product=test_product()
-                                                )
-            
+            order_item = OrderItem.objects.create(count=-1,
+                                                  order=self.test_order,
+                                                  product=self.test_product
+                                                  )
+
 
 class TransactionModelTest(TestCase):
-    
-        
-    def test_transaction_str(self):        
-        transaction = test_transaction()
+    def setUp(self) -> None:
+        self.test_user = get_user_model().objects.create_user('test@mail.com', 'Test1234')
+        self.test_address = Address.objects.create(user=self.test_user)
+        self.test_order = Order.objects.create(address=self.test_address, user=self.test_user)
+        self.test_transaction = Transaction.objects.create(user=self.test_user, order=self.test_order, transaction_code='1234', total_price=1)
+
+    def test_transaction_str(self):
+        transaction = self.test_transaction
         self.assertEqual(str(transaction), f'{transaction.user} : {transaction.transaction_code}')
-        
--- shop/tests/tests_models.py
+++ shop/tests/tests_models.py
@@ -3,5 +3,59 @@ from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
 from django.utils import timezone
 import datetime
+from django.utils.text import slugify
+
+from shop.models import Category, Product, MagicSale, Comment
 
 
+class CategoryModelTest(TestCase):
+    def setUp(self) -> None:
+        self.test_parent = Category('title', 'meta title', 'meta description', 'description')
+        self.test_category = Category('title', 'meta title', 'meta description', 'description', parent_category=self.test_parent)
+
+    def test_category_str(self):
+        category = self.test_category
+        self.assertEqual(str(category), f'{category.title}')
+
+    def test_parent_category(self):
+        self.assertEqual(self.test_category.parent_category, self.test_parent)
+
+
+class ProductModelTest(TestCase):
+    def setUp(self) -> None:
+        self.test_product = Product('name', 'brand', 1234, 1234)
+
+
+    def test_product_slug(self):
+        slug = slugify(self.test_product.name)
+        self.assertEqual(self.test_product.slug, slug)
+
+    def test_product_str(self):
+        self.assertEqual(str(self.test_product), self.test_product.name)
+
+
+class CommentModelTest(TestCase):
+    def setUp(self) -> None:
+        self.test_user = get_user_model().objects.create(email='mail@mail.com', password='Test1234')
+        self.test_product = Product('name', 'brand', 1234, 1234)
+        self.test_parent = Comment('content', 3, None, self.test_product, self.test_user)
+        self.test_comment = Comment('content', 5, parent_comment=self.test_parent, product=self.test_product, user=self.test_user)
+
+    def test_comment_str(self):
+        comment = self.test_comment
+        self.assertEqual(str(comment), f'{comment.content}')
+
+    def test_parent_category(self):
+        self.assertEqual(self.test_comment.parent_comment, self.test_parent)
+
+
+class MagicSaleModelTest(TestCase):
+    def setUp(self) -> None:
+        self.test_magic_sale = MagicSale('name')
+
+    def test_magi_sale_slug(self):
+        slug = slugify(self.test_magic_sale.name)
+        self.assertEqual(self.test_magic_sale.slug, slug)
+
+    def test_magic_sale_str(self):
+        self.assertEqual(str(self.test_magic_sale), self.test_magic_sale.name)
