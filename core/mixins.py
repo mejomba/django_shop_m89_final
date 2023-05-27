35d58bd90a4fc5d8cff328b60f001d3b25dfebfd
mejomba <moojaminzadeh@gmail.com>
2023-05-27 20:30:28 +0330

    add authenticated access denied mixin

--- core/mixins.py
+++ core/mixins.py
@@ -1,5 +1,5 @@
 
-
+from django.utils.text import gettext_lazy as _
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
 from django.contrib.auth import login, get_user_model
@@ -11,13 +11,27 @@ from rest_framework.response import Response
 from django.http import HttpResponseRedirect
 
 
-# class SuperUserOrStaffMixin(AccessMixin):
-#     def dispatch(self, request, *args, **kwargs):
-#         print(request.user, request.user.is_superuser , request.user.is_staff)
-#         if request.user and (request.user.is_superuser or request.user.is_staff):
-#             return super().dispatch(request, *args, **kwargs)
-#         messages.error(request, 'برای استفاده از این صفحه ابتدا وارد شوید.')
-#         return redirect('core_api:login')
+class AuthenticatedAccessDeniedMixin:
+    def dispatch(self, request, *args, **kwargs):
+        PROFILE_URL = reverse('core:profile')
+        LANDINGPAGE_URL = reverse('shop:landing_page')
+
+        if request.user.is_staff or request.user.is_superuser:
+            return redirect(PROFILE_URL)
+
+        if token := request.COOKIES.get('jwt'):
+            try:
+                payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+                user = get_user_model().objects.filter(id=payload['id']).first()
+                if user and user.is_active:
+                    request.user = user
+                    return redirect(PROFILE_URL)
+                if user and not user.is_active:
+                    messages.warning(request, _('حساب شما ایجاد شده اما هنوز فعال نشده، از ایمیل فعال سازی استفاده کنید'))
+                    return redirect(LANDINGPAGE_URL)
+            except Exception:
+                pass
+        return super().dispatch(request, *args, **kwargs)
 
 
 class StaffOrJwtLoginRequiredMixin(AccessMixin):
