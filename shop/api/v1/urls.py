a53ce5fe542413a46e7ed0efd32b5894a53322ff
mejomba <moojaminzadeh@gmail.com>
2023-06-01 23:45:18 +0330

    some minor fix and image upload

---
+++ shop/api/v1/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'shop_api'
+
+urlpatterns = [
+    # path('v1/cart/', views.CartAPI.as_view(), name='cart'),
+    path('v1/cart/<int:pk>/', views.CartItemAPI.as_view(), name='cart'),
+    path('v1/cart/', views.CartAPI.as_view(), name='cart2'),
+    path('v1/cart/submit/', views.CartAPI.as_view(), name='cart_to_order'),
+    path('v1/order/<int:pk>/', views.OrderAPI.as_view(), name='order_api'),
+    path('v1/order/', views.OrderListAPI.as_view(), name='order_list_api')
+    # path('cart/add/<int:pk>/', views.AddToCartAPI.as_view(), name='add_to_cart'),
+    # path('cart/delete/<int:pk>/', views.DeleteFromCartAPI.as_view(), name='delete_from_cart'),
+]
